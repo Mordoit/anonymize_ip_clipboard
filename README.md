2c53030631a49c833c85b06852912ff30bea321d
Mordoit <mordoit@protonmail.com>
2019-07-12 17:55:53 +0200

    version: 1.1.0

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Get the clipboard and modify all same ip by a other one.
 It could be use protect information before publish on internet.
 
 # Requirement
-`sudo apt-get install xsel`
-`pip3 install clipboard`
+> sudo apt-get install xsel
+
+> pip3 install pyperclip
--- modify_ip.py
+++ modify_ip.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python
 
 __author__ = "Mordoit"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __email__ = "mordoit@protonmail.com"
 
 # Import
 import re
-import clipboard
+import pyperclip
 
 input("Copy the text than you want to use\n\nPress Enter to continue...")
 
-# Create variable from the clipboard
-text = clipboard.paste()
+# Create variable from the pyperclip
+text = pyperclip.paste()
 
 # Get all IP in a list
 ips = re.findall(r'[0-9]+(?:\.[0-9]+){3}', text)
 
-# define the first number for the new address
-newip = 11
-
 for ip in ips:
+    new_ip += 11
     # Replace ip frome the list to the new ip
-    text = text.replace(ip, str(newip) + "." + str(newip) + "." + str(newip) + "." + str(newip) )
-    newip += 11
+    text = text.replace(ip, str(new_ip) + "." + str(new_ip) +
+                        "." + str(new_ip) + "." + str(new_ip))
 
 
-print("backup:\n\n" + clipboard.paste())
+print("backup:\n\n" + pyperclip.paste())
 
-print("\n--------------------\nModified data is in your clipboard")
-clipboard.copy(text)
+print("\n--------------------\nModified data is in your pyperclip")
+pyperclip.copy(text)
